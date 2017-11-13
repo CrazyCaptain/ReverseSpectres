3432aa776b251a676e7d5dcdc0e21c7d525c033b
DrSpaceInvader <masahud.ahmed@hotmail.com>
2017-11-13 10:25:30 +0800

    Add project files.

---
+++ ReverseSpectre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReverseSpectre.Startup))]
+namespace ReverseSpectre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
